ddaeec914ab8805a88e9a5d36f5561f5d020acfa
Seyedmahyar Emami <mayyxeng@gmail.com>
2020-08-14 17:35:20 +0200

    better plots and fixes to make files

--- hetero/utils/activityPlot.py
+++ hetero/utils/activityPlot.py
@@ -11,6 +11,18 @@ class ActorStat():
     self.total_cycles = int(element.attributes["clockcycles-total"].value)
     
     self.firings = int(element.attributes["firings"].value)
+
+    triggerElement = element.getElementsByTagName("trigger")[0]
+    self.trigger = {
+        "IDLE_STATE": int(triggerElement.attributes["IDLE_STATE"].value),
+        "LAUNCH": int(triggerElement.attributes["LAUNCH"].value),
+        "CHECK": int(triggerElement.attributes["CHECK"].value),
+        "SLEEP": int(triggerElement.attributes["SLEEP"].value),
+        "SYNC_LAUNCH": int(triggerElement.attributes["SYNC_LAUNCH"].value),
+        "SYNC_CHECK": int(triggerElement.attributes["SYNC_CHECK"].value),
+        "SYNC_WAIT": int(triggerElement.attributes["SYNC_WAIT"].value),
+        "SYNC_EXEC": int(triggerElement.attributes["SYNC_EXEC"].value)
+      }
   
   def getId(self):
     return self.id
@@ -20,6 +32,14 @@ class ActorStat():
   
   def getFirings(self):
     return self.firings
+  
+  def getSleeps(self):
+    return self.trigger["SLEEP"]
+  
+  def getSyncCycles(self):
+    return self.trigger["SYNC_EXEC"] + self.trigger["SYNC_WAIT"]
+
+
 
 def parseProfileXml(file_name):
 
@@ -36,22 +56,36 @@ def parseProfileXml(file_name):
 
   network_cycles = int(networkElement.attributes["clockcycles-total"].value)
 
-  labels = [actor.getId() for actor in actors]
-  activities = [actor.getCycles() / network_cycles for actor in actors]
-
-  x = [i for i in range(0, len(labels))]
+  labels = np.array([actor.getId() for actor in actors])
+  execs = np.array([actor.getCycles() / network_cycles for actor in actors])
+  sleeps = np.array([actor.getSleeps() / network_cycles for actor in actors])
+  syncs = np.array([actor.getSyncCycles() / network_cycles for actor in actors])
+  testWaits = np.array([1.0 for actor in actors]) - execs - sleeps - syncs 
+  print(testWaits)
+  print(execs)
+  print(labels)
+  width = 0.35
+  x = np.arange(len(labels))
 
   fig, ax = plt.subplots()
 
-  barplot = ax.bar(x, activities)
-  print(labels)
+  testWaitBars = ax.bar(x, testWaits, width, label='TEST*WAIT cycles', color='red')
+  sleepBars = ax.bar(x, sleeps, width, bottom=testWaits, label='SLEEP cycles', color='blue')
+  execBars = ax.bar(x, execs, width, bottom=testWaits + sleeps, label='TEST*EXEC cycles', color='green')
+  syncBars = ax.bar(x, syncs, width, bottom=testWaits + sleeps + execs, label='SYNC cycles', color='black')
+
   ax.set_xticks(x)
   ax.set_xticklabels(labels, rotation=90)
 
-  for bar in barplot:
-    height = bar.get_height()
-    ax.annotate("{:.2f}".format((height * 100.0)), xy=(bar.get_x() + bar.get_width() / 2, 
-    height), xytext=(0, 3), textcoords="offset points", ha='center', va='bottom')
+  def labelBars(bars):
+    for bar in bars:
+      height = bar.get_height()
+      ax.annotate("{:.2f}".format((height * 100.0)), xy=(bar.get_x() + bar.get_width() / 2, 
+      height), xytext=(0, 3), textcoords="offset points", ha='center', va='bottom')
+  ax.legend()
+  ax.yaxis.grid()
+  # labelBars(execBars)
+  # labelBars(sleepBars)
 
   plt.show()
   
