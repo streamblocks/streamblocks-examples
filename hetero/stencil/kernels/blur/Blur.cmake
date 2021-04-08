9216a6be43fe77b482bb37fa94e771375efcf6b1
mayyxeng <mayyxeng@gmail.com>
2021-04-08 19:54:22 +0200

    Added a long pipeline: File --> Jpeg decoder --> blur --> display

--- hetero/stencil/kernels/blur/Blur.cmake
+++ hetero/stencil/kernels/blur/Blur.cmake
@@ -18,3 +18,23 @@ streamblocks(
   SOURCE_PATH ${BitmapBlur_SOURCES}
   TARGET_PATH generated/test/BitmapBlurNetwork
 )
+
+
+set(JpegBlurNetwork_SOURCES
+  ${EXAMPLES_HOME}/hetero/stencil/bitmap
+  ${EXAMPLES_HOME}/converters/src
+  ${EXAMPLES_HOME}/system/art
+  ${EXAMPLES_HOME}/system/std
+  ${EXAMPLES_HOME}/jpeg/decoder/parallel
+  ${EXAMPLES_HOME}/jpeg/io
+  ${EXAMPLES_HOME}/hetero/stencil/kernels
+)
+
+streamblocks(
+  JpegBlurNetwork_MC
+  PLATFORM multicore
+  QID hetero.stencil.kernels.Blur.JpegBlurNetwork
+  SOURCE_PATH ${JpegBlurNetwork_SOURCES}
+  TARGET_PATH generated/test/JpegBlurNetwork
+)
+
